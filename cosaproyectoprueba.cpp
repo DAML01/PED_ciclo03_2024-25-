d9131649a23be9ea194623e992fb60d75c416b19
JuanJostian <164411525+JuanJostian@users.noreply.github.com>
2025-02-11 14:27:12 -0600

    Add files via upload

---
+++ cosaproyectoprueba.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctime>
+#include <string>
+using namespace std;
+
+struct DatosUsuario
+{
+    string nombreUsuario;
+    string contrasenhaUsuario;
+    int saldo;
+};
+
+struct Movimientos
+{
+    int Retiro;
+    int Deposito;
+    Movimientos *sig, *ant;
+};
+
+
